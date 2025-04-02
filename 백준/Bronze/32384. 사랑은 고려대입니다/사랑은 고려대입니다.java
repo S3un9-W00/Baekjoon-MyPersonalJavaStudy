515bca52f90dbfb430ab61eb3a9028f4f9d083ea
S3un9-W00 <anseunguwoo56@gmail.com>
2025-04-02 10:52:47 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 92 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int try1 = sc.nextInt();
+		
+		for(int i = 0; i < try1; i++) {
+			System.out.print("LoveisKoreaUniversity ");
+		}
+		
+		sc.close();
+	}
+}
