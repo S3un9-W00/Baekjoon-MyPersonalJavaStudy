a7f34addb77143c7dbe914925eb8ecd48787d118
S3un9-W00 <anseunguwoo56@gmail.com>
2025-04-02 12:32:38 +0900

    [Bronze III] Title: ACM 호텔, Time: 120 ms, Memory: 13776 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for(int i = 0; i < t; i++) {
+			int H = sc.nextInt();
+			int W = sc.nextInt();
+			int N = sc.nextInt();
+			
+			if(N % H == 0) {
+				System.out.println((H*100)+(N/H));
+			} else {
+				System.out.println(((N%H)*100) + ((N/H)+1));
+			}
+		}
+		
+		sc.close();
+	}
+}
