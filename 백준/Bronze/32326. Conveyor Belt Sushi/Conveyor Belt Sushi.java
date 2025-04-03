c746351d6e36239960be4511c212285fd18248a6
S3un9-W00 <anseunguwoo56@gmail.com>
2025-04-03 14:05:08 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 92 ms, Memory: 12808 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int red = sc.nextInt() * 3;
+		int green = sc.nextInt() * 4;
+		int blue = sc.nextInt() * 5;
+		
+		System.out.println(red + green + blue);
+	}
+}
