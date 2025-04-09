c52169146e89ef95e0eaf592e6ce0e7e9d9b6987
S3un9-W00 <anseunguwoo56@gmail.com>
2025-04-10 07:27:27 +0900

    [Bronze IV] Title: 방학 숙제, Time: 92 ms, Memory: 12788 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int L = sc.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+        
+        int day = 0;
+        
+        while(true){
+            if(A <= 0 && B <= 0){
+                break;
+            }
+            A -= C;
+            B -= D;
+            
+            day++;
+        }
+        System.out.println(L - day);
+    }
+}
